d04589ab408be712bde47960f7a8476a40a313e0
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-08-23 16:51:04 +0700

    Belajar Error Message Laravel Validation

--- tests/Feature/ValidationTest.php
+++ tests/Feature/ValidationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -43,5 +44,25 @@ class ValidationTest extends TestCase
 
         self::assertTrue($validator->fails());
         self::assertFalse($validator->passes());
+    }
+    public function testValidatorErrorMessage(): void
+    {
+        $data = [
+            'username' => '',
+            'password' => '',
+        ];
+        $rules = [
+            'username' => 'required',
+            'password' => 'required',
+        ];
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        self::assertTrue($validator->fails());
+        self::assertFalse($validator->passes());
+
+        $message = $validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
